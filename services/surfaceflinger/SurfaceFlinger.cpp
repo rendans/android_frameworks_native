fa3a59a2390e416132a259a889c2366ee94adfe9
rendan <rendans@gmail.com>
2016-01-01 19:04:46 +0700

    5

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -521,7 +521,7 @@ void SurfaceFlinger::init() {
 
     mEventControlThread = new EventControlThread(this);
     mEventControlThread->run("EventControl", PRIORITY_URGENT_DISPLAY);
-    android_set_rt_ioprio(mEventControlThread->getTid(), 1);
+/*    android_set_rt_ioprio(mEventControlThread->getTid(), 1);*/
 
     // set a fake vsync period if there is no HWComposer
     if (mHwc->initCheck() != NO_ERROR) {
